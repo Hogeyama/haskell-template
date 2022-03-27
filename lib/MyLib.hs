d47b1f600832b6b63a4fb379f394865b1d6f2cbf
Hogeyama <gan13027830@gmail.com>
2022-03-27 16:06:02 +0900

    Format

--- lib/MyLib.hs
+++ lib/MyLib.hs
@@ -1,10 +1,10 @@
 module MyLib (someFunc) where
-import           RIO
+
+import RIO
 
 -- | Some function
 --
 -- >>> someFunc
 -- someFunc
---
 someFunc :: IO ()
 someFunc = hPutBuilder stdout "someFunc"
