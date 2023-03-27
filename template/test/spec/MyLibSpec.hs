1786fe8c32bbe767b0b6f141e5de1338cc9f1344
Hogeyama <gan13027830@gmail.com>
2023-03-28 00:51:45 +0900

    Fix warnings

--- template/test/spec/MyLibSpec.hs
+++ template/test/spec/MyLibSpec.hs
@@ -3,9 +3,14 @@ module MyLibSpec (spec) where
 import MyLib
 import RIO
 import Test.Hspec
+import Test.QuickCheck (property)
 
 spec :: Spec
 spec = do
   describe "someFunc" $ do
     it "should return unit" $ do
       hello `shouldReturn` ()
+  describe "(+) @Int" $ do
+    it "is commutative" $ property $ do
+      \(x :: Int) (y :: Int) -> do
+        x + y `shouldBe` y + x
