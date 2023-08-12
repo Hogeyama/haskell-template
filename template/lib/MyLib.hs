dc8008b9c3f0e04616ef79673499b6fa00138ee2
Hogeyama <gan13027830@gmail.com>
2023-08-12 22:41:00 +0900

    feat: Add `silently` package for doctest

--- template/lib/MyLib.hs
+++ template/lib/MyLib.hs
@@ -2,9 +2,12 @@ module MyLib (hello) where
 
 import RIO
 
+-- $setup
+-- >>> import System.IO.Silently
+
 -- | Some function
 --
--- >>> hello
--- Hello World!
+-- >>> capture_ hello
+-- "Hello World!\n"
 hello :: IO ()
 hello = hPutBuilder stdout "Hello World!\n"
