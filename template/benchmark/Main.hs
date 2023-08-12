9782bcc2542ebcc0fb0b758bc679124e2da7c41b
Hogeyama <gan13027830@gmail.com>
2023-08-12 21:27:00 +0900

    style: Format benchmark/Main.hs

--- template/benchmark/Main.hs
+++ template/benchmark/Main.hs
@@ -1,15 +1,16 @@
 module Main (main) where
 
-import           Gauge.Main
-import           Gauge.Main.Options
-import           MyLib              ()
-import           RIO
-
+import Gauge.Main
+import Gauge.Main.Options
+import MyLib ()
+import RIO
 
 main :: IO ()
-main = defaultMainWith config
+main =
+  defaultMainWith
+    config
     [ bench "const" $ whnf const ()
-    --, bench "someFunc" $ whnfIO someFunc
+    -- , bench "someFunc" $ whnfIO someFunc
     ]
   where
     config = defaultConfig
