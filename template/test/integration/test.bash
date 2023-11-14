ab4597f050769bb323d84f6fc158662c7dc36181
Hogeyama <gan13027830@gmail.com>
2023-11-15 03:22:03 +0900

    fix: Minor fixes to test script

--- template/test/integration/test.bash
+++ template/test/integration/test.bash
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 set -euxo pipefail
-# Usage:
-# Run `arion up -d webserver` before running this script.
 
 # Input
-: HOST="${HOST:=localhost:3000}"
+: "${HOST:=localhost:3000}"
 
 output=$(curl -sSf "http://$HOST/hello/world")
 if [[ "$output" = "Hello world!" ]]; then
