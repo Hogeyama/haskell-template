006a7ad2e4936100c61f7ed9013650c83ceeaa51
Hogeyama <gan13027830@gmail.com>
2023-11-15 23:47:44 +0900

    feat: Update sample app to use postgres

--- template/test/integration/test.bash
+++ template/test/integration/test.bash
@@ -4,10 +4,17 @@ set -euxo pipefail
 # Input
 : "${HOST:=localhost:3000}"
 
-output=$(curl -sSf "http://$HOST/hello/world")
-if [[ "$output" = "Hello world!" ]]; then
-  echo "Found expected output 'Hello world!'"
-else
-  echo "Unexpected output '$output'"
-  exit 1
-fi
+# User投入
+curl -sSf -X POST "http://$HOST/users/taro?age=15"
+# User取得
+user=$(curl -sSf "http://$HOST/users/taro")
+diff -u <(jq <<<"$user" '.name') <(jq -n '"taro"')
+diff -u <(jq <<<"$user" '.age')  <(jq -n '15')
+
+# Blog投入
+curl -sSf -X POST "http://$HOST/users/taro/blog_posts?title=Hello"
+curl -sSf -X POST "http://$HOST/users/taro/blog_posts?title=World"
+# Blog取得
+blogs=$(curl -sSf "http://$HOST/users/taro/blog_posts")
+diff -u <(jq <<<"$blogs" '.[0]') <(jq -n '"Hello"')
+diff -u <(jq <<<"$blogs" '.[1]') <(jq -n '"World"')
